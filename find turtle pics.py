8198cde5f3a66ef9447073b6a4461a79b5939e4e
Lucian <73092546+Flyingbacen@users.noreply.github.com>
2024-04-04 20:37:37 -0400

    it looks better now (cleaned)

--- find turtle pics.py
+++ find turtle pics.py
@@ -5,34 +5,28 @@ import io
 import urllib.request
 import tkinter as tk
 
-# Load your JSON data
 with open(r'list.json') as f:
     data = json.load(f)
 
-# Function to replace spaces with dashes and search the JSON data
 def search_and_update_list(search_term):
     search_term = search_term.replace(" ", "-")
     matching_results = [item for item in data if search_term in item["name"]]
     listbox.update(values=matching_results)
 
-# Event handler for listbox selection
 def on_listbox_selection(event):
     selected_items = listbox.get()
-    if not selected_items:  # Check if any item is selected
+    if not selected_items:
         return
 
-    selected_item = selected_items[0]  # Assuming you only want the first selected item
+    selected_item = selected_items[0]
     image_url = selected_item["url"]
 
-    # Load the image from the URL
     image_data = urllib.request.urlopen(image_url).read()
     image = Image.open(io.BytesIO(image_data))
 
-    # Display the image on the right column
     photo = ImageTk.PhotoImage(image)
     image_elem.update(data=photo)
 
-    # Copy the image URL to clipboard
     root = tk.Tk()
     root.withdraw()
     root.clipboard_clear()
@@ -40,7 +34,6 @@ def on_listbox_selection(event):
     root.update()
 
 
-# Define the layout
 layout = [
     [
         sg.Text("Search:"),
@@ -55,10 +48,8 @@ layout = [
     ]
 ]
 
-# Create the window
 window = sg.Window("Image Search App", layout, resizable=True, finalize=True)
 
-# Get the elements for event updates
 listbox = window["_LISTBOX_"]
 image_elem = window["_IMAGE_"]
 
@@ -74,5 +65,4 @@ while True:
     elif event == "_LISTBOX_":
         on_listbox_selection(event)
 
-# Close the window
 window.close()
