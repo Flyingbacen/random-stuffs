0da70d6492bac341f97652390612e63b40a82a4f
aster <thisgacha@gmail.com>
2024-07-25 02:32:14 -0400

    set an option to disable a check, useful for not having to reposition the coords everytime (not tested)

--- ps99 scripts/chest open.py
+++ ps99 scripts/chest open.py
@@ -3,20 +3,22 @@ import pyautogui
 import time
 
 def main():
-    ecoords = 800, 510
+    ecoords = 800, 510 # set to (0, 0) to ignore the button check
     okcoords = 690, 725
     ecolor = (255, 255, 255)
     okcolor = (125, 245, 13)
     try:
         while True:
-            if pyautogui.pixelMatchesColor(*ecoords, ecolor, tolerance=10):
+            if pyautogui.pixelMatchesColor(*ecoords, ecolor, tolerance=10) or ecoords == (0, 0):
                 pydirectinput.press("e")
                 time.sleep(0.3)
-                if pyautogui.pixelMatchesColor(*okcoords, okcolor, tolerance=10):
-                    pydirectinput.click(*okcoords, 10, 0.1)
-                    time.sleep(4)
-                else:
-                    print(str(pyautogui.pixel(*okcoords)))
+                while True:
+                    if pyautogui.pixelMatchesColor(*okcoords, okcolor, tolerance=10):
+                        pydirectinput.click(*okcoords, 10, 0.1)
+                        time.sleep(4)
+                        break
+                    else:
+                        break
             time.sleep(0.5)
     except pydirectinput.FailSafeException:
         print("Program ended")
