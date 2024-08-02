f3a30320cebf2ef598c980946715009feb1c5b2a
aster <thisgacha@gmail.com>
2024-08-02 09:47:47 -0400

    added hotkeys (F3 to start, F8 to stop)

--- ps99 scripts/chest open.py
+++ ps99 scripts/chest open.py
@@ -1,14 +1,18 @@
 import pydirectinput
 import pyautogui
 import time
+import threading
+import keyboard
 
 def main():
     ecoords = (0, 0) # 800, 510 # set to (0, 0) to ignore the button check
     okcoords = 690, 725
     ecolor = (255, 255, 255)
     okcolor = (125, 245, 13)
+    global running
+    running = True
     try:
-        while True:
+        while running:
             if pyautogui.pixelMatchesColor(*ecoords, ecolor, tolerance=10) or ecoords == (0, 0):
                 pydirectinput.press("e")
                 time.sleep(0.3)
@@ -24,6 +28,15 @@ def main():
         print("Program ended")
         return
 
+def stop():
+    global running
+    running = False
+    main_thread.join()
+    print("Exited")
+    exit()
+
 if __name__ == "__main__":
-    time.sleep(2) # 5 seconds to switch to the game
-    main()
+    main_thread = threading.Thread(target=main)
+    print("Press F3 to start, F8 to stop")
+    keyboard.add_hotkey("f3", lambda: (main_thread.start(), keyboard.add_hotkey("f8", stop)))
+    keyboard.wait("f8")
